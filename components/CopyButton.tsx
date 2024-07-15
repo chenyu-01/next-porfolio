f34632d8c212efd171760f8df90b8baa1b34de48
chenyu <chenyu101@outlook.com>
2024-07-16 03:30:32 +0800

    feat: auto revert copy button after 2s

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -1,16 +1,22 @@
 'use client';
 import React, { useState } from 'react';
 import { Copy, CopyCheck } from 'lucide-react';
-
 export default function CopyButton({ value }: { value: string }) {
   const [copied, setCopied] = useState(false);
+  function copyToClipboard(value: string) {
+    navigator.clipboard.writeText(value);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  }
+
   return (
     <>
       {!copied && (
         <Copy
           onClick={() => {
-            navigator.clipboard.writeText(value);
-            setCopied(true);
+            copyToClipboard(value);
           }}
           className="h-4 w-4"
         />
