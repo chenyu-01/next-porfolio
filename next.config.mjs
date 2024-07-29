25b361f2285e75a16129fc942fc41f1f56dad0f6
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-07-29 13:19:38 +0800

    🦄 refactor(upgrade next version): next15, react19

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import withTM from 'next-transpile-modules';
 
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default withTM(['react-syntax-highlighter'])(nextConfig);
