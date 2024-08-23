3baa4ce5406970df3def850383e7c2ae2d7e2ab0
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-08-23 18:27:39 +0800

    ✨ feat(homepage): add welcom message
    homescreen adding left text part

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-// home page of the app, with about section
+import { Button } from '@/components/ui/button';
+
 const Page = () => {
-  return <p>hello world</p>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+      <div className="w-full max-w-2xl rounded-lg bg-white p-6 font-bold shadow-md">
+        <h1 className="mb-4 text-4xl">
+          Hi, I am a Frontend Developer With Vue, React and Typescript
+        </h1>
+        <div className="flex space-x-4">
+          <Button variant={'outline'}>Certificates</Button>
+          <Button>Resume</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default Page;
