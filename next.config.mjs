96c61aff0549d7fdaee9a6d174caefe58478ca58
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-09-24 14:56:54 +0800

    Revert "🦄 refactor(upgrade next version): next15, react19"
    This reverts commit 25b361f2285e75a16129fc942fc41f1f56dad0f6.

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import withTM from 'next-transpile-modules';
 
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    reactCompiler: true,
-  },
 };
 
 export default withTM(['react-syntax-highlighter'])(nextConfig);
